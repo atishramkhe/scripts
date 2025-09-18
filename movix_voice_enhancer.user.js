23d3b297724986eaab00bb6d22ef155173df0e75
Atish Ramkhelawon <atishramkhelawon@gmail.com>
2025-09-18 16:13:55 +0200

    Add update and download URLs to the user script
    Added update and download URLs for the script.

--- movix_voice_enhancer.user.js
+++ movix_voice_enhancer.user.js
@@ -7,6 +7,8 @@
 // @match        https://movix.website/*
 // @allFrames    true
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/atishramkhe/scripts/refs/heads/main/movix_voice_enhancer.user.js
+// @downloadURL  https://raw.githubusercontent.com/atishramkhe/scripts/refs/heads/main/movix_voice_enhancer.user.js
 // ==/UserScript==
 
 (function() {
