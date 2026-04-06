ed2c9688eaaa1099a98318f2ddc51be2a1f399dd
Atish Ramkhelawon <atishramkhelawon@gmail.com>
2026-04-06 19:30:52 +0400

    Update version to 4.9 and add new script URL
    corrected movies search bug over 1st results' poster

--- github_master_loader.user.js
+++ github_master_loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        GitHub Master Script Loader (URL-Aware with Caching)
 // @namespace   atishramkhe
-// @version     4.8
+// @version     4.9
 // @description Loads and executes other Violentmonkey scripts from your GitHub repository, respecting their @match/@include/@exclude directives, with caching.
 // @author      Ateaish
 // @match       *://*/*
@@ -32,7 +32,8 @@
         'https://raw.githubusercontent.com/atishramkhe/scripts/refs/heads/main/cdn_autoplay.user.js', 
         'https://raw.githubusercontent.com/atishramkhe/scripts/refs/heads/main/cdn_adblock.user.js', 
         'https://raw.githubusercontent.com/atishramkhe/scripts/refs/heads/main/ateaish_drama_helpers.user.js', 
-        'https://raw.githubusercontent.com/atishramkhe/scripts/refs/heads/main/ateaish_anime_player_automation.user.js'
+        'https://raw.githubusercontent.com/atishramkhe/scripts/refs/heads/main/ateaish_anime_player_automation.user.js', 
+        'https://raw.githubusercontent.com/atishramkhe/scripts/refs/heads/main/ateaish_autoplay.user.js'
 
         // Add more script URLs here as needed
     ];
